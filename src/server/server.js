61638af3d89cf48dc3c00a830480ab73ae758ae0
Gokulan Dhakshinamoorthy <41450940+dgokul19@users.noreply.github.com>
2022-06-30 13:20:19 +0530

    Update server.js (#1)

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,7 @@ const app = require('express')();
 const express = require('express');
 const cors = require('cors');
 const bodyparser = require('body-parser');
+const port = 8080;
 
 app.use(express.json());
 app.use(cors());
@@ -10,5 +11,4 @@ app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 
 
-const port = 8080;
 app.listen(port, () => console.log('Server Started on Port', port));
